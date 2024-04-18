25cf3cc640cae5104bf2027a40673d4de23900e0
PaganiAlejandro <alejandropagani466@gmail.com>
2024-04-18 10:41:52 -0300

    Add bottom navigation with navcontroller

--- app/src/main/java/com/alepagani/blogapp/MainActivity.kt
+++ app/src/main/java/com/alepagani/blogapp/MainActivity.kt
@@ -2,28 +2,50 @@ package com.alepagani.blogapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.alepagani.blogapp.core.hide
+import com.alepagani.blogapp.core.show
+import com.alepagani.blogapp.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
+        observeDestinationChange(navController)
+    }
 
+    private fun observeDestinationChange(navController: NavController) {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 }
-
-// val db = FirebaseFirestore.getInstance()
-// db.collection("ciudades").document("LA").get().addOnSuccessListener {document ->
-//     document?.let {
-//         Log.d("Firebase", "Datos: ${it.data}")
-//     }
-// }.addOnFailureListener { error ->
-//     Log.e("Firebase error", "Error: ${error?.message}")
-// }
